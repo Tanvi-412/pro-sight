c38176b44b9c4e03b07eddbe928431388ea43196
Tanvi <thanamsh@steven.edu>
2020-08-09 20:39:26 -0400

    added from email  file

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -3,7 +3,7 @@
     $visitor_email = $POST['email'];
     $message = $_POST['message'];
 
-    $email_from = 'tanvihanamshet412@gmail.com';
+    $email_from = 'thanamsh@stevens.edu';
     $email_subject = "Pro-Sight Information Required!";
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
