389669268317d6a4fffee4f4f8fd0f460beb0786
Tanvi <thanamsh@steven.edu>
2020-08-15 17:18:05 -0400

    added from email  file

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,19 +1,23 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $POST['email'];
-    $message = $_POST['message'];
+    if (isset($POST['submit'])){
+        $name = $_POST['name'];
+        $visitor_email = $POST['email'];
+        $message = $_POST['message'];
 
-    $email_from = 'tanvihanamshet412@gmail.com';
-    $email_subject = "Pro-Sight Information Required!";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
 
-    $to = "tanvihanamshet412@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-    mail($to, $email_subject, $email_body, $headers);
-    header("Location:index.html");
+        $to = "thanamsh@stevens.edu";
+        $headers = "From: $email_from \r\n";
+        $headers .= "Reply-To: $visitor_email \r\n";
+        $email_subject = "Pro-Sight Information Required!";
+        $email_body = "User Name: $name.\n"."User Email: $visitor_email.\n"."User Message: $message.\n";
 
+
+
+        mail($to, $email_subject, $email_body, $headers);
+        header("Location: index.html?mailsend");
+    }
+    
+   
+    
 
 ?>
